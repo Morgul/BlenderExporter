9dc4481f60d2122ad4036a4b0de5ce06fd37cad3
Christopher S. Case <chris.case@g33xnexus.com>
2026-03-22 20:58:20 -0500

    Version 4.2.0

--- src/babylon_js/__init__.py
+++ src/babylon_js/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'Babylon.js',
     'author': 'David Catuhe, Jeff Palmer',
-    'version': (3, 3, 2),
+    'version': (4, 2, 0),
     'blender': (4, 2, 0),
     'location': 'File > Export > Babylon.js (.babylon)',
     'description': 'Export Babylon.js scenes (.babylon)',
